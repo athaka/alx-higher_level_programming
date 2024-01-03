06872c5b3fb7224ee6ccb096606f941e3f403029
athaka <jesoka18@gmail.com>
2024-01-04 00:04:27 +0100

    last is not always list

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+last = abs(number) % 10
+
+output_format = f"Last digit of {number} is {last} and is"
+
+if last > 5:
+    output_format += " greater than 5"
+elif last == 0:
+    output_format += " 0"
+else:
+    output_format +=  " less than 6 and not 0"
+
+print(output_format)
