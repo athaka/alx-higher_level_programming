961357b61c5982a187e6f65d13649ebcfe972ce0
athaka <jesoka18@gmail.com>
2024-01-03 23:37:24 +0100

    when you see the signs

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+if number > 0:
+    print(number, end=" is positive\n")
+elif number == 0:
+    print(number, end=" is zero\n")
+else:
+    print(number, end=" is negative\n")
