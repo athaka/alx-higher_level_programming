d28211507425b618ddaaac5d151add9300800ab1
athaka <jesoka18@gmail.com>
2024-01-04 01:03:29 +0100

    hexadecimals are for hackers

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Author - Jonathan Esokawu
+
+"""Print numbers 0 to 98 in decimal and hexadecimal, respectively"""
+
+for num in range(0, 99):
+    print("{} = {}".format(num, hex(num)))
