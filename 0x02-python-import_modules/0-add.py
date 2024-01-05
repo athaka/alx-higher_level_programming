58aab866d31702c46c1eaa18ca300450b8652155
athaka <jesoka18@gmail.com>
2024-01-05 01:08:15 +0100

    import a fxn from a file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 # Author: Jonathan Esokawu
 
+from add_0 import add
+
 a = 1
 b = 2
-
-from add_0 import add
 
 result = add(a, b)
 
 print("{} + {} = {}".format(a, b, result))
-
