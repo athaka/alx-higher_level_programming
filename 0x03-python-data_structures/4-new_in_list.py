bc2083fb3bd42854b2377536ada0938d6d5b4088
athaka <jesoka18@gmail.com>
2024-01-09 04:36:07 +0100

    reverse a list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author: Jonathan Esokawu
+
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    new = my_list.copy()
+    new[idx] = element
+    return new
